e9d75808dceda7f94bbc955a3ac70ac7e3d128cb
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-02-03 16:12:06 +0800

    对话框文本修正

--- src/utils/dialog.js
+++ src/utils/dialog.js
@@ -3,7 +3,11 @@ import Vue from 'vue'
 export function Confirm (text, title) {
   return Vue.prototype.$dialog.confirm({
     text,
-    title
+    title,
+    actions: [
+      { text: '否', color: 'black', key: false },
+      { text: '是', color: 'blue', key: true }
+    ]
   })
 }
 
